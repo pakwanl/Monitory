b1f6b01ebea581312f0ade9736b6f91ea4dd7b3a
onionboo <164863244+onionboo@users.noreply.github.com>
2024-07-10 13:21:23 +0700

    Update v_____0_____1.py

--- v_____0_____1.py
+++ v_____0_____1.py
@@ -303,12 +303,6 @@ if 'scraped_data' in st.session_state:
         output = BytesIO()
         writer = pd.ExcelWriter(output, engine='xlsxwriter')
         filtered_data.to_excel(writer, index=False, sheet_name=f"{current_datetime_ymd}")
-        workbook = writer.book
-        worksheet = writer.sheets['Sheet1']
-        format1 = workbook.add_format({'num_format': '0.00'}) 
-        worksheet.set_column('A:A', None, format1)  
-        writer.save()
-        processed_data = output.getvalue()
         return processed_data
     
     current_datetime_ymd = datetime.datetime.now(pytz.timezone('Asia/Bangkok')).strftime("%Y-%m-%d")
