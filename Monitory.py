cb537ee96c8059ca3431d37aefc1da91065a9508
onionboo <164863244+onionboo@users.noreply.github.com>
2024-07-19 10:08:16 +0700

    Update Monitory.py

--- Monitory.py
+++ Monitory.py
@@ -9,14 +9,10 @@ import time
 from urllib.parse import urljoin
 import datetime
 import pytz
-import subprocess
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
+import json
+import xlsxwriter
+from io import BytesIO
+from streamlit_option_menu import option_menu
 
 #### ----------------setting----------------- ####
 
@@ -25,13 +21,13 @@ hide_st_style = """
     #MainMenu {visibility: hidden;}
     footer {visibility: hidden;}
     header {visibility: hidden;}
-    </style> 
+    </style>
     """
 st.markdown(hide_st_style, unsafe_allow_html = True)
 
 #### -------------Tableau connect------------ ####
 
-    ## Disabled Tableau Function (SSLErrors)
+## Disabled Tableau Function (SSLErrors)
 
 #### -----------function definition---------- ####
 
@@ -41,7 +37,7 @@ def cleanText(text):
     newText = ' '.join(newText.split())  # Keep only one white space
     return newText
 
-def get_text_html(url):
+def get_all_text(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
@@ -52,47 +48,6 @@ def get_text_html(url):
     except requests.exceptions.SSLError as e:
         return f"SSL error for URL '{url}': \n{e}"
 
-def get_text_java(url):
-    options = Options()
-    options.add_argument('--headless')
-
-    # Ensure webdriver_manager is installed and up to date
-    try:
-        result = subprocess.run(["pip", "show", "webdriver-manager"], capture_output=True, text=True)
-        if "webdriver-manager" not in result.stdout:
-            print("Installing webdriver-manager...")
-            subprocess.run(["pip", "install", "webdriver-manager"])
-        else:
-            current_version = result.stdout.split("\n")[1].split(": ")[-1]
-            if current_version < "0.8.3":
-                print(f"Upgrading webdriver_manager (current: {current_version})...")
-                subprocess.run(["pip", "install", "--upgrade", "webdriver-manager"])
-    except subprocess.CalledProcessError as e:
-        print(f"Error checking/upgrading webdriver_manager: {e}")
-
-    # Ensure Chrome is installed
-    try:
-        result = subprocess.run(["which", "google-chrome"], capture_output=True, text=True)
-        if not result.stdout.strip():
-            print("Chrome not found. Please install Chrome or set the appropriate environment variable.")
-            return ""
-    except subprocess.CalledProcessError:
-        print("Error checking Chrome installation. Please ensure Chrome is installed.")
-        return ""
-
-    try:
-        driver_path = ChromeDriverManager().install()
-        driver = webdriver.Chrome(service=Service(driver_path), options=options)
-        driver.get(url)
-        page_source = driver.page_source
-        soup = BeautifulSoup(page_source, 'html.parser')
-        return soup.get_text()
-    except Exception as e:
-        return f"An error occurred: {e}"
-    finally:
-        if 'driver' in locals():
-            driver.quit()
-
 def get_pdf(url):
     try:
         response = requests.get(url)
@@ -102,7 +57,7 @@ def get_pdf(url):
         pdf_urls = []
         for link in links:
             href = link.get('href')
-            if href and href.lower().endswith(('.pdf')): # <- add more type of file available here
+            if href and href.lower().endswith('.pdf'):
                 download_url = urljoin(url, href)
                 pdf_urls.append(download_url)
         return pdf_urls
@@ -125,17 +80,14 @@ def extract_matches(text, patterns):
 
 uploaded_file = st.file_uploader("Upload here :lightning_cloud:", type=None, accept_multiple_files=False)
 if uploaded_file is not None:
-    try:
-        url = pd.read_excel(uploaded_file, sheet_name="product")
-        url = pd.DataFrame(url)
+    url = pd.read_excel(uploaded_file, sheet_name="product")
+    url = pd.DataFrame(url)
 
-        patterns = pd.read_excel(uploaded_file, sheet_name="pattern")
-        patterns = pd.DataFrame(patterns)
+    patterns = pd.read_excel(uploaded_file, sheet_name="pattern")
+    patterns = pd.DataFrame(patterns)
 
-        st.write("Product", url)
-        st.write("Keyword pattern", patterns)
-    except Exception as e:
-        st.error(f"An error occurred while reading the file: {e}")
+    st.write("Product", url)
+    st.write("Keyword pattern", patterns)
 else:
     st.warning(":receipt: uploading file to continue!")
 
@@ -148,7 +100,6 @@ if uploaded_file is not None:
         globals()[set_name] = {key: fr"{value}" for key, value in pattern_dict.items()}
         
     url = url[url.Status == 'keep']
-    url = url.reset_index(drop=True)
 
 #### --------------web scraping-------------- ####
 
@@ -165,26 +116,12 @@ def scrape_data(url, unique_set):
         current_datetime = datetime.datetime.now(pytz.timezone('Asia/Bangkok')).strftime("%Y-%m-%d %H:%M:%S")
         pdf_files = get_pdf(rl)
         pdf.append('\n\n- '.join(pdf_files) if pdf_files else 'x')
-        
-        if url.loc[idx, 'scrapable'] == 'Java':
-            scrape = get_text_java(rl)
-            if scrape:
-                ws.append([scrape])
-            else:
-                ws.append([""])
-            timestamp.append(current_datetime)
-            time.sleep(5)
-            progress_bar.progress(int((idx + 1) * progress_step))
-        else:
-            scrape = get_text_html(rl)
-            if scrape:
-                ws.append([scrape])
-            else:
-                ws.append([""])
-            timestamp.append(current_datetime)
-            time.sleep(5)
-            progress_bar.progress(int((idx + 1) * progress_step))
-            
+        scrape = get_all_text(rl)
+        ws.append([scrape])
+        timestamp.append(current_datetime)
+        time.sleep(5)
+        progress_bar.progress(int((idx + 1) * progress_step))
+
     cleaned = []
     for web in ws:
         for text in web:
@@ -192,17 +129,16 @@ def scrape_data(url, unique_set):
             cleaned.append(clean)
 
     url['Manual-Fact-Sale Sheet'] = pdf
-    
-    # qc = []
-    # for row in url['Manual-Fact-Sale Sheet']:
-    #     if row == 'x':
-    #         qc.append('FALSE')
-    #     else:
-    #         qc.append('TRUE')
+    ability = []
+    for row in url['Manual-Fact-Sale Sheet']:
+        if row == 'x':
+            ability.append('FALSE')
+        else:
+            ability.append('TRUE')
 
     url['timestamp'] = timestamp
     url['scraped'] = cleaned
-    # url['QC'] = qc
+    # url['ability'] = ability
 
     url_ = url[url.Status != 'x']
     url_ = url_.reset_index(drop=True)
@@ -217,7 +153,7 @@ def scrape_data(url, unique_set):
         scraped = row['scraped']
         product = row['Product_Name']
         status = row['Status']
-        u_rl = row['URL']
+        url = row['URL']
         pdf = row['Manual-Fact-Sale Sheet']
         timestamp = row['timestamp']
         clean_ws = cleanText(scraped)
@@ -230,7 +166,7 @@ def scrape_data(url, unique_set):
                 "FI_type": type_,
                 "Status" : status,
                 "product_type": product_type,
-                "URL": u_rl,
+                "URL": url,
                 "PDF": pdf,
                 "timestamp": timestamp,
                 "keywords": []
