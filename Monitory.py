fbb3336d533b7083747fb5b69e18eff5dc88e9ba
onionboo <164863244+onionboo@users.noreply.github.com>
2024-09-25 11:09:38 +0700

    Update Monitory.py

--- Monitory.py
+++ Monitory.py
@@ -30,14 +30,14 @@ import google.generativeai as genai
 
 #### ----------------setting----------------- ####
 
-hide_st_style = """
-    <style>
-    # MainMenu {visibility: hidden;}
-    # footer {visibility: hidden;}
-    header {visibility: hidden;}
-    </style> 
-    """
-st.markdown(hide_st_style, unsafe_allow_html = True)
+# hide_st_style = """
+#     <style>
+#     # MainMenu {visibility: hidden;}
+#     # footer {visibility: hidden;}
+#     header {visibility: hidden;}
+#     </style> 
+#     """
+# st.markdown(hide_st_style, unsafe_allow_html = True)
 
 #### -----------function definition---------- ####
 
